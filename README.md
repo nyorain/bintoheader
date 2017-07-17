c4dde91e902b5eabb21b2f53b6510f4c2e152e0e
nyorain <nyorain@gmail.com>
2017-07-17 20:52:37 +0200

    Remove CMake; fix meson building

--- README.md
+++ README.md
@@ -1,34 +1,38 @@
 ### Binary To Header File
 
 This repository contains the code for a tiny program that is able to convert a (binary) file
-into a valid C or C++ header file holding an array with the files data.
-This can be useful for including e.g. images, audio or shader files directly into the binary in a
-platform independent way.
+into a valid C or C++ header file holding an array with the file's data.
+This can be useful for including e.g. images, audio or compiled shader files directly into the
+binary in a platform independent way.
 
-To use it, just build bintoheader.cpp with a C++11 compiler (Should work with GCC 5/6, Clang and
-MSVC 2013/2015. Only tested on GCC 5/6). 
+To use it, just build bintoheader.cpp with a C++1z compiler. Currently only working using g++-7.
 You can also simply use the provided makefile to build (make), install ([sudo] make install) or clean (make clean) it.
-To configure compiler or install path just change the makefile, it is not that scary (13 lines
-actually).
-Note that getting it to compile with clang might require some extra include and link flags depending on the clang installation.
+To configure compiler or install path just change the makefile, it is not that scary.
+Alternatively use the provided meson project (e.g. as subproject).
 
 ## Usage:
 
-``````````````````````````````
-  bintoheader [OPTION...]
+```
+Transforms a file into a c/cpp header holding its binary data as array.
+Valid Usages:
 
-  -i, --input arg   the input file. Can be any type of file, binary as well
-                    as text
-  -o, --output arg  the output file. By default the input flie name with a
-                    '.h' suffix
-  -s, --size arg    the byte size of the array {8|16|32|64}. Defaulted to 8.
-                    Will fail if file size is not multiple.
-  -n, --name arg    the name of the array in the c header. Defaulted to
-                    'binaryData'
-  -h, --help        Print this help string
-````````````````````````````
+	bintoheader <INPUT> [<OUTPUT>] [<ARGS>]
+	bintoheader [<ARGS>]
 
-This will convert the input file into the valid C header specified by the output argument that 
-contains an array with the raw data of the input file.
+The following arguments are recognized:
+
+	-i	The input file
+	-o	The output file [defaulted to '<input>.h']
+	-name	The name of the c array
+		Defaulted to '<input>.h', with all '.' replaced by '.'
+	-size	The size of the array values. Defaulted to 8.
+		The file size must be a multiple of the size value
+	-help	Show this error message
+
+The program is released into the public domain
+A copy of the source can be obtained under
+https://github.com/nyorain/bintoheader
+```
+
 The header can then be used with C++ (for C++11 and later it will define a constexpr array) or
 with C99, in which case it will simply define a constant array.
--- bintoheader.cpp
+++ bintoheader.cpp
@@ -1,10 +1,10 @@
 // This is free and unencumbered software released into the public domain.
-// 
+//
 // Anyone is free to copy, modify, publish, use, compile, sell, or
 // distribute this software, either in source code form or as a compiled
 // binary, for any purpose, commercial or non-commercial, and by any
 // means.
-// 
+//
 // In jurisdictions that recognize copyright laws, the author or authors
 // of this software dedicate any and all copyright interest in the
 // software to the public domain. We make this dedication for the benefit
@@ -12,7 +12,7 @@
 // successors. We intend this dedication to be an overt act of
 // relinquishment in perpetuity of all present and future rights to this
 // software under copyright law.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -20,7 +20,7 @@
 // OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-// 
+//
 // For more information, please refer to <http://unlicense.org>
 
 #include <fstream>
@@ -31,67 +31,17 @@
 #include <stdexcept>
 #include <cstring>
 
-#include "cxxopts.hpp"
+#include "flags.hpp"
 
-constexpr auto cxxoptsLicense = 1 + R"STR(
-Copyright (c) 2014, 2015, 2016 Jarryd Beck
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-)STR";
-
-constexpr auto unilicense = 1 + R"STR(
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org>
-)STR";
-
-//beginning of the file to output
+// file template to output
 constexpr auto fileTemplate = 1 + R"STR(
-#ifndef BINARY_%i_HEADER_INCLUDE
-#define BINARY_%i_HEADER_INCLUDE
+#ifndef BINARY_%i_H_INC
+#define BINARY_%i_H_INC
 
 #include <stdint.h>
 
-//just check for c++11
 #if defined(__cplusplus) && __cplusplus >= 201103L
-	constexpr	
+	constexpr
 #else
 	const
 #endif
@@ -100,14 +50,13 @@ constexpr auto fileTemplate = 1 + R"STR(
 	%d
 };
 
-#endif //header guard)STR";
+#endif // header guard)STR";
 
-void replace(std::string& str, const std::string& from, const std::string& to) 
+void replace(std::string& str, const std::string& from, const std::string& to)
 {
     if(from.empty()) return;
     size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) 
-	{
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
     }
@@ -121,8 +70,7 @@ std::vector<std::uint8_t> loadFile(const std::string& file)
 	auto openmode = std::ios::ate | std::ios::binary;
 
 	std::ifstream ifs(file, openmode);
-	if(!ifs.is_open()) 
-	{
+	if(!ifs.is_open()) {
 		std::cerr << "loadFile: couldnt open input file " << file << "\n";
 		std::cerr << "error: " << std::strerror(errno) << "\n";
 		exit(EXIT_FAILURE);
@@ -133,8 +81,7 @@ std::vector<std::uint8_t> loadFile(const std::string& file)
 
 	std::vector<std::uint8_t> buffer(size);
 	auto data = reinterpret_cast<char*>(buffer.data());
-	if(!ifs.read(data, size)) 
-	{
+	if(!ifs.read(data, size)) {
 		std::cerr << "loadFile: failed to read input file " << file << "\n";
 		std::cerr << "error: " << std::strerror(errno) << "\n";
 		exit(EXIT_FAILURE);
@@ -143,21 +90,19 @@ std::vector<std::uint8_t> loadFile(const std::string& file)
 	return buffer;
 }
 
-void outputData(const std::vector<std::uint8_t>& data, const std::string& file, 
+void outputData(const std::vector<std::uint8_t>& data, const std::string& file,
 	const std::string& name, int size)
 {
 	auto fname = file;
 	std::ofstream ofs(fname);
-	if(!ofs.is_open()) 
-	{
+	if(!ofs.is_open()) {
 		std::cerr << "outputData: couldnt open output file " << file << "\n";
 		std::cerr << "error: " << std::strerror(errno) << "\n";
 		exit(EXIT_FAILURE);
 	}
 
 	auto byteSize = size / 8;
-	if(data.size() % byteSize) 
-	{
+	if(data.size() % byteSize) {
 		std::cerr << "outputData: file size is not a multiple of specified integer size\n";
 		std::cerr << "\tfile size: " << data.size() << " bytes\n";
 		std::cerr << "\tspecified size: " << size / 8 << " bytes\n";
@@ -168,8 +113,7 @@ void outputData(const std::vector<std::uint8_t>& data, const std::string& file,
 	dataString.reserve(data.size() * 5); //approx.
 
 	auto currColSize = 0u;
-	for(auto i = 0u; i < data.size() / byteSize; ++i)
-	{
+	for(auto i = 0u; i < data.size() / byteSize; ++i) {
 		std::uint64_t value = 0;
 		auto ptr = &data[i * byteSize];
 
@@ -180,7 +124,7 @@ void outputData(const std::vector<std::uint8_t>& data, const std::string& file,
 
 		auto str = std::to_string(value);
 		if(i < data.size() / byteSize - 1) str += ", "; //check if last value or not
-		if(currColSize + str.size() > lineLength) 
+		if(currColSize + str.size() > lineLength)
 		{
 			dataString.append("\n\t");
 			currColSize = 0;
@@ -204,70 +148,60 @@ void outputData(const std::vector<std::uint8_t>& data, const std::string& file,
 
 int main(int argc, char** argv)
 {
-	//parse args
-	std::string input;
-	std::string output;
-	std::string name = "binaryData";
-	unsigned int size = 8;
+	// parse args
+	const flags::args args(argc, argv);
 
-	try
-	{
-		cxxopts::Options options("bintoheader", "Program to convert files into C headers");
+	// help
+	auto help = args.get<bool>("help", false);
+	if(help) {
+		std::cout << "Transforms a file into a c/cpp header holding its binary data as array.\n";
+		std::cout << "Valid Usages:\n\n";
+		std::cout << "\tbintoheader <INPUT> [<OUTPUT>] [<ARGS>]\n";
+		std::cout << "\tbintoheader <ARGS>\n\n";
+		std::cout << "The following arguments are recognized:\n\n";
+		std::cout << "\t-i\tThe input file\n";
+		std::cout << "\t-o\tThe output file [defaulted to '<input>.h']\n";
+		std::cout << "\t-name\tThe name of the c array\n";
+		std::cout << "\t\tDefaulted to '<input>.h', with all '.' replaced by '.'\n";
+		std::cout << "\t-size\tThe size of the array values in bits. Defaulted to 8.\n";
+		std::cout << "\t\tThe file size must be a multiple of the size value\n";
+		std::cout << "\t-help\tShow this error message\n\n";
+		std::cout << "The program is released into the public domain\n";
+		std::cout << "A copy of the source can be obtained under\n";
+		std::cout << "https://github.com/nyorain/bintoheader\n";
+		return 0;
+	}
 
-		options.add_options()
-			("i,input", "the input file. Can be any type of file, binary as well as text",
-				cxxopts::value<std::string>(input))
-			("o,output", "the output file. By default the input flie name with a '.h' suffix", 
-				cxxopts::value<std::string>(output))
-			("s,size", "the byte size of the array {8|16|32|64}. Defaulted to 8. Will fail if "
-				"file size is not multiple.", cxxopts::value<unsigned int>(size))
-			("n,name", "the name of the array in the c header. Defaulted to 'binaryData'", 
-				cxxopts::value<std::string>(name))
-			("l,license", "outputs the license of this software and used software")
-			("h,help", "Print this help string");
+	// input, output
+	auto input = args.get<std::string>("i", {});
+	auto output = args.get<std::string>("o", {});
+	const auto& pos = args.positional();
 
-		options.parse(argc, argv);
-		if(options.count("help"))
-		{
-			std::cerr << options.help({""}) << "\n";
-			return EXIT_SUCCESS;
-		}
+	auto current = 0u;
+	if(input.empty() && pos.size() > current)
+		input = pos[current++];
 
-		if(options.count("license"))
-		{
-			std::cerr << "\n";
-			std::cerr << "The License of the bintoheader itself:\n";
-			std::cerr << "======================================\n\n";
-			std::cerr << unilicense << "\n\n";
+	if(output.empty() && pos.size() > current)
+		output = pos[current++];
 
-			std::cerr << "The License of cxxopts, used to parse arguments:\n";
-			std::cerr << "================================================\n\n";
-			std::cerr << cxxoptsLicense << "\n";
-			return EXIT_SUCCESS;
-		}
-		
-		if(options.count("input") != 1)
-		{
-			std::cerr << "Invalid usage.\n" << options.help({""}) << "\n";
-			return EXIT_FAILURE;
-		}
+	if(output.empty())
+		output = input + ".h";
 
-		if(!(size == 8 || size == 16 || size == 32 || size == 64))
-		{
-			std::cerr << "Invalid size paramter.\n";
-			return EXIT_FAILURE;
-		}
-
-		if(options.count("output") == 0) output = input + ".h";
+	// name, size
+	auto name = args.get<std::string>("name", {});
+	if(name.empty()) {
+		name = input + "_data";
 		replace(name, ".", "_");
-	} 
-	catch(const cxxopts::OptionException& e)
-	{
-		std::cerr << "Could not parse given arguments\n";
-			return EXIT_FAILURE;
+	}
+
+	auto size = args.get<unsigned int>("size", 8);
+	if(size != 8 && size != 16 && size != 32 && size != 64) {
+		std::cout << "Invalid size parameter\n";
+		std::cout << "Must be one of {8, 16, 32, 64}\n";
+		return 1;
 	}
 
 	auto data = loadFile(input);
 	outputData(data, output, name, size);
-	std::cout << "Succesfully parsed " << name << ". " << data.size () << " bytes\n";
+	std::cout << "Succesfully created " << name << ": " << data.size () << " bytes\n";
 }
